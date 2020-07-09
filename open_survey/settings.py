98c9d6c1e50c1caca9fb2bd92b2c2e0641e5106b
Bastian Greshake Tzovaras <bgreshake@googlemail.com>
2020-07-09 09:47:19 +0200

    allow hosts in debug=false

--- open_survey/settings.py
+++ open_survey/settings.py
@@ -29,7 +29,7 @@ SECRET_KEY = os.getenv("SECRET_KEY")
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = os.getenv("DEBUG", "False").lower() == "true"
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["*"]
 
 ON_HEROKU = os.getenv("ON_HEROKU", "False").lower() == "true"
 
